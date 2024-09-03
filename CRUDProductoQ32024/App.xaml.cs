ccd14e24f486f1bee89dbe25df8b4c0097b5b6fb
gurdian0614 <ivan.gurdian@outlook.com>
2024-09-02 19:54:19 -0600

    Add project files.

---
+++ CRUDProductoQ32024/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CRUDProductoQ32024
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
